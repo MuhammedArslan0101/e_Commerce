b5f6e91bba744279b49161c8355790ef44d8d081
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-09 14:38:24 +0300

    update Css

---
+++ e_Commerce/Migrations/202104091122230_EditDataInitializer.cs
@@ -0,0 +1,16 @@
+﻿namespace e_Commerce.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditDataInitializer : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ e_Commerce/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace e_Commerce.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<e_Commerce.Entity.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "e_Commerce.Entity.DataContext";
+        }
+
+        protected override void Seed(e_Commerce.Entity.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
