ddde650dcc1f8402b53ce6aad202d42fd0a733d1
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-05-10 00:51:54 +0300

    Owin Class

---
+++ e_Commerce/App_Start/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(e_Commerce.App_Start.Startup1))]
+
+namespace e_Commerce.App_Start
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions {
+            AuthenticationType ="ApplicationUser" , LoginPath = new PathString ("/Account/Login")
+            
+            });
+        }
+    }
+}
