7ac23c658753855df7cc71df615d15ad99a6f0ae
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-05-11 00:49:34 +0300

    Add Products To Cart

---
+++ e_Commerce/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using e_Commerce.Entity;
+using e_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Commerce.Controllers
+{
+    public class CartController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.products.FirstOrDefault(i => i.Id == Id);
+            //Add product to Cart from database 
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+
+
+            return RedirectToAction("Index");
+
+        }
+        // Create cart For User for first Time 
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                // Add new cart To session
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
