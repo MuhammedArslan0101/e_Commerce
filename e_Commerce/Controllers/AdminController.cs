7de629b13c2b278502fc8c9f87114b30b292bd11
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-05-17 23:11:11 +0300

    Order Status Statistics (Admin Panel ) Count carts

---
+++ e_Commerce/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using e_Commerce.Entity;
+using e_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Commerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            StateModel model = new StateModel();
+            model.ExpectedOrderCount = db.orders.Where(i => i.OrderState == OrderState.Expected).ToList().Count();
+            model.CompletedOrderCount = db.orders.Where(i => i.OrderState == OrderState.Completed).ToList().Count();
+            model.PackegedOrderCount = db.orders.Where(i => i.OrderState == OrderState.Packaged).ToList().Count();
+            model.ShippedOrderCount = db.orders.Where(i => i.OrderState == OrderState.Shipped).ToList().Count();
+
+            model.ProductCount = db.products.Count();
+            model.OrdertCount = db.orders.Count();
+
+            return View(model);
+        }
+    }
+}
---
+++ e_Commerce/Models/StateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_Commerce.Models
+{
+    public class StateModel
+    {
+        public int ProductCount { get; set; }
+        public int OrdertCount { get; set; }
+        public int ExpectedOrderCount { get; set; }
+
+        public int CompletedOrderCount { get; set; }
+
+        public int PackegedOrderCount { get; set; }
+        public int ShippedOrderCount { get; set; }
+
+
+
+
+    }
+}
